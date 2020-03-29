864e441a3f0e13031cdeb7d03953cf591adf9149
saikiran <35389534+sai1004@users.noreply.github.com>
2020-03-29 10:44:04 +0530

    Update covid.sql

--- covid.sql
+++ covid.sql
@@ -2,6 +2,66 @@ create database covid_19;
 
 use covid_19;
 
+create table covid_network (
+
+    id varchar(299) primary key not null,
+    profile_id varchar(299) not null 
+
+);
+profile_id --> profile
+
+create table profile (
+
+    id varchar(299) primary key not null,
+    contacted_to_id varchar(299) not null
+
+);
+contacted_to_id --> suspects
+
+create table suspects (
+
+    id varchar(299) primary key not null,
+    name varchar(255) not null,
+    age int(3) not null,
+    gender enum('F','M','Other') not null,
+    mobile int(16),
+    is_screened boolean not null,
+    is_quarantine boolean not null,
+    tested_on datetime default now(),
+    covid_result boolean default null,
+    travel_history varchar(299) not null,
+    updated_on timestamp default now(),
+
+);
+
+create table infected_locations (
+
+    	id varchar(299) not null,
+    	area varchar(299) not null,
+    	land_mark varchar(255),
+    	city varchar(255),
+    	state varchar(255),
+    	country varchar(255),
+    	zip_code int(10),
+    	lat varchar(255) not null default '00.0000000',
+    	lng varchar(255) not null default '00.0000000',
+    	infected_on datetime not null,
+    	is_sanitized boolean not null default false,
+    	show_map boolean not null default true,
+	img_id varchar(30) not null
+    	updated_on timestamp default now(),
+
+);
+img_id --> infected_locations_img_id
+
+create table infected_locations_img (
+    
+    	id varchar(99) not null,
+    	name varchar(99) default 'upload',
+    	src longtext not null
+
+);
+
 create table covid_paitent (
 
 	id varchar(299) primary key not null,
@@ -55,73 +115,16 @@ address_id --> address
 
 create table covid_documents (
 
-    id varchar(299) primary key not null,
-    scanned_docs_id varchar(299) not null,
+    	id varchar(299) primary key not null,
+    	scanned_docs_id varchar(299) not null,
 
 );
 scanned_docs_id --> img
 
 create table address (
 	
-    id varchar(299) primary key not null,
-    area varchar(255),
-    land_mark varchar(255),
-    city varchar(255),
-    state varchar(255),
-    country varchar(255),
-    zip_code int(10),
-    lat varchar(255) not null default '00.0000000',
-    lng varchar(255) not null default '00.0000000',
-    show_map boolean not null default true,
-    updated_on timestamp default now(),
-
-);
-
-create table img (
-    
-    id varchar(99) primary key,
-    name varchar(99) default 'upload',
-    src longtext not null
-
-);
-
-create table covid_network (
-
-    id varchar(299) primary key not null,
-    profile_id varchar(299) not null 
-
-);
-profile_id --> profile
-
-create table profile (
-
-    id varchar(299) primary key not null,
-    contacted_to_id varchar(299) not null
-
-
-);
-contacted_to_id --> suspects
-
-create table suspects (
-
-    id varchar(299) primary key not null,
-    name varchar(255) not null,
-    age int(3) not null,
-    gender enum('F','M','Other') not null,
-    mobile int(16),
-    is_screened boolean not null,
-    is_quarantine boolean not null,
-    tested_on datetime default now(),
-    covid_result boolean default null,
-    travel_history varchar(299) not null,
-    updated_on timestamp default now(),
-
-);
-
-create table infected_locations (
-
-    	id varchar(299) not null,
-    	area varchar(299) not null,
+    	id varchar(299) primary key not null,
+    	area varchar(255),
     	land_mark varchar(255),
     	city varchar(255),
     	state varchar(255),
@@ -129,19 +132,17 @@ create table infected_locations (
     	zip_code int(10),
     	lat varchar(255) not null default '00.0000000',
     	lng varchar(255) not null default '00.0000000',
-    	infected_on datetime not null,
-    	is_sanitized boolean not null default false,
     	show_map boolean not null default true,
-	img_id varchar(30) not null
     	updated_on timestamp default now(),
 
 );
-img_id --> infected_locations_img_id
 
-create table infected_locations_img (
+create table img (
     
-    id varchar(99) not null,
-    name varchar(99) default 'upload',
-    src longtext not null
+    	id varchar(99) primary key,
+    	name varchar(99) default 'upload',
+    	src longtext not null
 
 );
+
+
